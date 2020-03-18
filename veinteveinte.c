689edff48ffe70c237d1a5e85273eb83a61aac3d
Karen Palacio <karenpalacio@gmail.com>
2020-03-18 18:11:30 -0300

    un cacho de pseudocodigo para las funciones

--- veinteveinte.c
+++ veinteveinte.c
@@ -1,11 +1,36 @@
 #include "veinteveinte.h"
 
 u32 Greedy(Grafo G){
+    //si no podemos modificar el grafo G, copiarlo antes y modificar la copia
     //en el orden pasar por cada nodo
     //si es el primero ponerle color 0
     //al siguiente, fijarse si ponerle el color min interfiere
     //con el coloreo que ya está en el grafo
     //darle el color min tal que no joda
 
-    return ;
+    return color;
+};
+
+//preguntar si greedy y el resto de las cosas modifican al grafo que se le pasa 
+//OJO
+//PREGUNTAR TIPO DEL RETURN
+//usar el del apunte >:(
+u32 Bipartito(Grafo G) {
+    //asumiendo que el reordenamiento disminuye numcrom (y no lo aumenta)
+    for i do n times:
+        numCrom[i] = greedy(G);
+        AleatorizarOrden(G, R);
+    //calcular greedy de G en varios órdenes
+    colorMin = min(numCrom)
+    if colorMin == 2 return 1;
+    else return 0;
+};
+
+u32 NumCC(Grafo G) {
+    //calcular bnfs para un nodo. guardar los nodos de su comp conexa
+    //calcular bnfs para el siguiente nodo que no está en alguna comp
+    //conexa ya calculada.
+    //seguir hasta que no haya más nodos a recorrer
+    //la cant de estos conjuntos de comp conexas va a ser la cant de comp conexas de G
+    return 0;
 };
