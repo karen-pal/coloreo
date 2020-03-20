b40ae40681330d247d66dbfd99ea6ef551b2cdaa
Karen Palacio <karenpalacio@gmail.com>
2020-03-20 17:46:51 -0300

    mejor grafo para probar greedy.py

--- greedy.py
+++ greedy.py
@@ -1,10 +1,18 @@
-nodos_ejemplo = [{"vecinos": [1,2], "nombre":0, "grado":2, "color":"nada"},
-        {"vecinos": [2,0], "nombre":1, "grado":2, "color":"nada"},
-        {"vecinos": [1,0], "nombre":2, "grado":2, "color":"nada"}
+nodos_ejemplo = [{"vecinos": [1], "nombre":0, "grado":1, "color":"nada"},
+        {"vecinos": [0], "nombre":1, "grado":1, "color":"nada"},
+        {"vecinos": [3], "nombre":2, "grado":1, "color":"nada"},
+        {"vecinos": [2], "nombre":3, "grado":1, "color":"nada"}
         ]
 
-grafo_ejemplo = {"cant_vert" : 3, "cant_lados" : 3, "delta":2,"nodos":nodos_ejemplo, "orden":[0,1,2]}
+grafo_ejemplo = {"cant_vert" : 4, "cant_lados" : 2, "delta":1,"nodos":nodos_ejemplo, "orden":[0,1,2,3]}
 
+"""
+es el grafo
+0°---°1
+
+2°---°3
+
+"""
 
 def greedy(g):
     idx = g["orden"][0]
@@ -13,16 +21,14 @@ def greedy(g):
     #nodo["color"]=0
     #print(nodo)
     g["nodos"][0]["color"]=0
-    for i in range(len(nodo["vecinos"])):
+    for i in nodo["vecinos"]:
         g["nodos"][i]["color"]=1
-    """
+
     for i in range(g["cant_vert"]):
         idx = g["orden"][i]
         nodo = g["nodos"][idx]
 
         print(nodo["color"])
-    """
-    print(g)
 
 
 
