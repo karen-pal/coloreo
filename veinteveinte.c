fc9026c477a560c8c014829a152c0a4933500b9a
Karen Palacio <karenpalacio@gmail.com>
2020-03-24 14:01:51 -0300

    dejo la impl de mila para que la pensemos, empiezo a construir arriba

--- veinteveinte.c
+++ veinteveinte.c
@@ -1,34 +1,8 @@
 #include "veinteveinte.h"
 
 u32 Greedy(Grafo G){
-    //inicializacion
-    nodos_array[0].color = 0;
-    color_0.append(nodos[0]);
-    for i in (1, length(nodos_array[0].vecinos)) {
-        nodos_array[0].vecino[i].color = 1;
-        color_i.append(nodos_array[0].vecino[i])
-    }
-    /*
-    arreglo dinamico con True..el grado del grafo sería
-    el largo del arreglo??
-    */
-    for i in cant_vert,i=1, i++ {
-        siguiente_nodo = Nodo(orden[i])
-        for j in (0,i-1), j++ {
-        if siguiente_nodo in Nodo(orden[i-1]).vecinos {
-            if siguiente_nodo.color = Nodo(orden[i-1]).color
-            siguiente_nodo.color = Nodo(orden[i-1]).color + 1;
-            break;
-        }
 
-    }
-    }
-}
-    
-
-u32 Greedy(Grafo G){
-
-    G->nodos_array[0].color = 0;
+    GetNodeByName(G,Nombre(G,0)).color = 0;
     u32 actual_color;
     for (u32 i = 1; i < G->cant_ver; i++) {
         actual_color = 0;
@@ -44,35 +18,13 @@ u32 Greedy(Grafo G){
         }
         G->nodos_array[i].color = actual_color;
     }
-}
-
-
-
-
-
-
-
-
-
-    for i in length(nodos_array of G) {
-        for (j in 0 i-1, j++) {
-            if nodos_array[i].nombre not in nodos_array[j].vecinos and nodos_array[i].color==nodos_array[j].color {
-                break;
-            }
-        }
-        nodos_array[i].color = j
-        
-o bien no es vecino del mas chico, entonces tiene el color mas chico
-o es vecino
-
-    }
     //en el orden pasar por cada nodo
     //si es el primero ponerle color 0
     //al siguiente, fijarse si ponerle el color min interfiere
     //con el coloreo que ya está en el grafo
     //darle el color min tal que no joda
 
-    return color;
+    return actual_color;
 };
 
 //preguntar si greedy y el resto de las cosas modifican al grafo que se le pasa 
