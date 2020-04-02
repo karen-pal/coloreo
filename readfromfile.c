60057264bee7790ebf9d13fc98398acc5301354f
Karen Palacio <karenpalacio@gmail.com>
2020-04-02 19:41:43 -0300

    cleanup

--- readfromfile.c
+++ readfromfile.c
@@ -3,8 +3,8 @@
 
 int line_count(FILE *file){
     int lines = 0;
-    //in_file = fopen(file, "r");
     rewind(file);
+
     char buff_lett[100];
     char buff_num[200];
     char buff_num2[200];
@@ -25,13 +25,13 @@ int main(void) {
         printf("ERROR READING FILE\n");
         return 0;
     }
+
     char buff_lett[100];
     char buff_num[200];
     char buff_num2[200];
-    fscanf(in_file, "%s %s %s", buff_lett, buff_num ,buff_num2);
-    printf("%s %s %s\n", buff_lett,buff_num,buff_num2);
     int a;
     int lines = 0;
+
     while (( a = fscanf(in_file, "%s %s %s", buff_lett, buff_num, buff_num2)) != EOF) {
         printf("%d\n", a);
         lines++;
@@ -40,17 +40,11 @@ int main(void) {
             printf("wtf\n");
         }
     }
+
     printf("%d\n", lines);
     lines = line_count(in_file);
 
     printf("CON LINECOUNT %d\n", lines);
-    /*
-    printf("%s\n", buff);
-    int i;
-    fscanf(in_file, "%d", &i);
-    printf("%d\n",i);
-    fscanf(in_file, "%d", &i);
-    printf("%d\n",i);*/
     fclose(in_file);
     return 0;
 }
