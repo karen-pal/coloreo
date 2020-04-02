7e299a0d37b8651da8911ea6003119c3cb6a5853
Karen Palacio <karenpalacio@gmail.com>
2020-04-02 19:18:58 -0300

    entendiendo scanf

--- readfromfile.c
+++ readfromfile.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 int lines(FILE *file){
     int lines = 0;
     in_file = fopen(file, "r");
@@ -11,7 +11,7 @@ int lines(FILE *file){
     }
     return lines;
 }
-
+*/
 
 int main(void) {
     char letter;
@@ -22,13 +22,17 @@ int main(void) {
         printf("ERROR READING FILE\n");
         return 0;
     }
-    char buff[100];
-    fscanf(in_file, "%s", buff);
+    char buff_lett[100];
+    char buff_num[200];
+    char buff_num2[200];
+    fscanf(in_file, "%s %s %s", buff_lett, buff_num ,buff_num2);
+    printf("%s %s %s\n", buff_lett,buff_num,buff_num2);
+    /*
     printf("%s\n", buff);
     int i;
     fscanf(in_file, "%d", &i);
     printf("%d\n",i);
     fscanf(in_file, "%d", &i);
-    printf("%d\n",i);
+    printf("%d\n",i);*/
     return 0;
 }
