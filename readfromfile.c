565bad7ca7903db994c4793f1d10ea7f4dfd776e
Karen Palacio <karenpalacio@gmail.com>
2020-04-02 19:24:34 -0300

    wow re entiendo fscanf ahora

--- readfromfile.c
+++ readfromfile.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+
 int lines(FILE *file){
     int lines = 0;
-    in_file = fopen(file, "r");
-    while (scanf(file, "%s %d %d", &buff)==1) {
-        if (buff=="\n"){
+    //in_file = fopen(file, "r");
+    char buff_lett[100];
+    char buff_num[200];
+    char buff_num2[200];
+    while (fscanf(file, "%s %s %s", buff_lett, buff_num, buff_num2) != EOF) {
+        printf("%s %s %s\n", buff_lett,buff_num,buff_num2);
+        if (buff_lett=="\n"){
             lines++;
         }
     }
     return lines;
 }
-*/
+
 
 int main(void) {
     char letter;
@@ -27,6 +31,8 @@ int main(void) {
     char buff_num2[200];
     fscanf(in_file, "%s %s %s", buff_lett, buff_num ,buff_num2);
     printf("%s %s %s\n", buff_lett,buff_num,buff_num2);
+    int line_num = lines(in_file);
+    printf("%d\n", line_num);
     /*
     printf("%s\n", buff);
     int i;
