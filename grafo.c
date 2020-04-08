597e1de3e6e2cb2a5616a9971b84c20a7ed9b6d9
Karen Palacio <karenpalacio@gmail.com>
2020-04-08 18:16:41 -0300

    recuperar numeros de más de 1 dígito
    Committer: Karen Palacio <karen.palacio.1994@gmail.com>

--- grafo.c
+++ grafo.c
@@ -29,25 +29,23 @@ int main(void) {
     int file_length = line_count(in_file);
     char *file_array[file_length];
     int i = 0;
-
     while (fgets(line,8,in_file) != NULL) {
         //puts(line);
         i++;
         file_array[i] = line;
+        printf("de largo %ld\n", strlen(file_array[i]));
+        for (int j = 1; j<strlen(file_array[i]); j++) {
+            if (isdigit(file_array[i][j]) && isdigit(file_array[i][j-1])) {
+                int digit1 = (int) file_array[i][j-1] - 48;
+                int digit2 = (int) file_array[i][j] - 48;
+                printf("el numero completo es %d%d\n", digit1, digit2);
+            }
+        }
         int first_node_name = (int)file_array[i][2]-48;
         int second_node_name = (int)file_array[i][4]-48;
         printf("%d %d\n",first_node_name, second_node_name);
     }
 
     fclose(in_file);
-    /* no sé acceder al array of arrays.... nice!
-    for (int j=0;j<file_length;j++) {
-        for (int k=0; k<strlen(file_array[j]); k++) {
-            if (isdigit(file_array[j][k])) {
-                printf("%d",file_array[j][k]);
-            }
-        }
-    }
-    */
     return(0);
 }
