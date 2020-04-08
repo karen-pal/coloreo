d9660450bb94b96d29623bdee6b9e3ee2c856223
Karen Palacio <karenpalacio@gmail.com>
2020-04-08 18:23:48 -0300

    darle formato, corregir error de contador
    Committer: Karen Palacio <karen.palacio.1994@gmail.com>

--- grafo.c
+++ grafo.c
@@ -29,11 +29,11 @@ int main(void) {
     int file_length = line_count(in_file);
     char *file_array[file_length];
     int i = 0;
+
     while (fgets(line,8,in_file) != NULL) {
-        //puts(line);
-        i++;
         file_array[i] = line;
-        printf("de largo %ld\n", strlen(file_array[i]));
+        printf("una linea de largo %ld:\n", strlen(file_array[i]));
+
         for (int j = 1; j<strlen(file_array[i]); j++) {
             if (isdigit(file_array[i][j]) && isdigit(file_array[i][j-1])) {
                 int digit1 = (int) file_array[i][j-1] - 48;
@@ -41,11 +41,14 @@ int main(void) {
                 printf("el numero completo es %d%d\n", digit1, digit2);
             }
         }
+
         int first_node_name = (int)file_array[i][2]-48;
         int second_node_name = (int)file_array[i][4]-48;
         printf("%d %d\n",first_node_name, second_node_name);
+        i++;
     }
 
     fclose(in_file);
+
     return(0);
 }
