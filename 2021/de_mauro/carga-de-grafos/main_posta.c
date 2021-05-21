3192f5a1d254a55258aff48ed3f4eba0a963808f
Karen Palacio <karen.palacio.1994@gmail.com>
2021-05-21 17:55:05 -0300

    empezar a implementar el paso 6

--- 2021/de_mauro/carga-de-grafos/main_posta.c
+++ 2021/de_mauro/carga-de-grafos/main_posta.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 #include "RomaVictor.h"
 #include "requeridas.h"
 #include <stdbool.h>
 #include <time.h>
 #include <stdlib.h>
+
 typedef
     struct GResult {
         u32 result;
@@ -23,6 +23,34 @@ int _gresult_compare(const void *_a, const void *_b) {
     else
         return 1;
 }
+
+void AleatorizarPerm(u32 * perm, u32 R, u32 length){
+    srand(R);
+    u32 *array_random = calloc(length,sizeof(u32));
+    if (array_random == NULL){
+        return;
+    }
+    u32 i = 0;
+    for (int k=0; k<length;k++){
+        array_random[k]=k;
+    }
+    for (u32 i = 0; i < length * 10; i++) {
+        u32 p1 = rand() % length;
+        u32 p2 = rand() % length;
+        u32 v1 = array_random[p1];
+        u32 v2 = array_random[p2];
+        array_random[p1] = v2;
+        array_random[p2] = v1;
+    }
+    for (u32 i = 0; i < length; i++) {
+        perm[i] = array_random[i];
+    }
+
+    free(array_random);
+    array_random=NULL;
+    return;
+}
+
 int main(int argc, char *argv[]) {
     int counter;
     printf("Program Name Is: %s\n",argv[0]);
@@ -43,7 +71,7 @@ int main(int argc, char *argv[]) {
     int b = atoi(argv[2]);
     int c = atoi(argv[3]);
     int d = atoi(argv[4]);
-    int r = atoi(argv[5]);
+    int e = atoi(argv[5]);
     int f = atoi(argv[6]);
     Grafo grafo = ConstruccionDelGrafo();
     printf("\n");
@@ -62,7 +90,7 @@ int main(int argc, char *argv[]) {
     // paso 4
     //ya taba
     // paso 5
-
+    //TODO: ordenamiento natural
     GResult * greedys_result = calloc(a,sizeof(GResult));
     for (int j=0; j< a; j++){
         greedys_result[a].result = 0;
@@ -71,10 +99,10 @@ int main(int argc, char *argv[]) {
     }
     printf("Te aviso que hago %d greedys...\n",a);
     for (int i = 0; i<a; i++){
-        AleatorizarVertices(grafo,i);
+        AleatorizarVertices(grafo,f+i);
         greedys_result[i].result = Greedy(grafo);
-        greedys_result[i].seed = i;
-        printf("seed %d greedy da %u\n",i, greedys_result[i].result);
+        greedys_result[i].seed = f+i;
+        printf("seed %d greedy da %u\n",greedys_result[i].seed, greedys_result[i].result);
     }
     qsort(greedys_result,a, sizeof(GResult),_gresult_compare);
     GResult best_result = greedys_result[0];
@@ -86,6 +114,26 @@ int main(int argc, char *argv[]) {
     u32 check = Greedy(grafo);
     printf("Debería ser que %d (encontrado iterando)\n     es igual a %u\n", best_X,check);
     // paso 6
+    u32 * perm = calloc(best_X,sizeof(u32));
+    for (int i=0; i<best_X; i++){
+        perm[i]=0;
+    }
+    u32 bloque_result;
+    for (u32 i = 0; i<b; i++){
+        AleatorizarPerm(perm, f+i, best_X);
+        printf("Te muestro perm........\n");
+        //for(int j=0; j<best_X; j++){
+        //    printf("perm[%d]=%u\n",j,perm[j]);
+        //}
+        OrdenPorBloqueDeColores(grafo, perm);
+        bloque_result = Greedy(grafo);
+        printf("Orden con perm %u dio como resultado %u\n",f+i, bloque_result);
+        if (bloque_result>best_X){
+            printf("Error... %u > %u.\n> Exiting ...", bloque_result,best_X);
+            return 1;
+        }
+    }
+    
 
     free(greedys_result);
     DestruccionDelGrafo(grafo);
